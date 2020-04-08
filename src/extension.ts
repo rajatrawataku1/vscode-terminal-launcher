1073b222a66e04a082c7e569280f4868e03d1aff
Rajat Rawat <rajat.rawat@flipkart.com>
2020-04-08 21:05:22 +0530

    added split capabilties

--- src/extension.ts
+++ src/extension.ts
@@ -1,158 +1,229 @@
-import * as fs from 'fs';
-import * as os from 'os';
-import * as path from 'path';
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
 import * as vscode from "vscode";
 
-import { Config, getConfig } from './config';
+import { Config, getConfig } from "./config";
 import {
-	commandNameInputBoxOptions,
-	commandScriptInputBoxOptions,
-	projectNameInputBoxOptions
-} from './inputBoxOptions';
-import {
-	Project,
-	ProjectStorage,
-	TerminalCommand
-} from './storage';
+  groupNameInputBoxOptions,
+  commandNameInputBoxOptions,
+  commandScriptInputBoxOptions,
+  projectNameInputBoxOptions,
+} from "./inputBoxOptions";
+import { Project, ProjectStorage, TerminalCommand } from "./storage";
+import { create } from "domain";
 
 const homeDir = os.homedir();
 const PROJECTS_FILE = `terminal-projects.json`;
 
+const enum USER_OPTIONS {
+  "ADD_COMMANDS" = "Add Commands",
+  "ADD_GROUP" = "Add Group",
+  "CANCEL" = "CANCEL",
+}
 // this method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-	const configurations: Config = getConfig();
-	const projectStorage: ProjectStorage = new ProjectStorage(getProjectFilePath());
-	const errorLoading: string = projectStorage.load();
+  const configurations: Config = getConfig();
+  const projectStorage: ProjectStorage = new ProjectStorage(
+    getProjectFilePath()
+  );
+  const errorLoading: string = projectStorage.load();
 
-	vscode.commands.registerCommand(`terminalLauncher.saveProject`, () => saveProject());
-	vscode.commands.registerCommand(`terminalLauncher.editProjects`, () => editProjects());
-	vscode.commands.registerCommand(`terminalLauncher.runProject`, () => runProject());
+  vscode.commands.registerCommand(`terminalLauncher.saveProject`, () =>
+    saveProject()
+  );
+  vscode.commands.registerCommand(`terminalLauncher.editProjects`, () =>
+    editProjects()
+  );
+  vscode.commands.registerCommand(`terminalLauncher.runProject`, () =>
+    runProject()
+  );
 
-	function saveProject(): void {
-		const projectName = vscode.workspace.rootPath.substr(vscode.workspace.rootPath.lastIndexOf("/") + 1);
-		const projectPath = vscode.workspace.rootPath;
+  function saveProject(): void {
+    const projectName = vscode.workspace.rootPath.substr(
+      vscode.workspace.rootPath.lastIndexOf("/") + 1
+    );
+    const projectPath = vscode.workspace.rootPath;
 
-		vscode.window.showInputBox(projectNameInputBoxOptions(projectName))
-			.then(async _projectName => {
-				if (typeof _projectName === "undefined") {
-					return;
-				}
-				if (_projectName === "") {
-					vscode.window.showWarningMessage("You must define a name for the project.");
-					return;
-				}
+    vscode.window
+      .showInputBox(projectNameInputBoxOptions(projectName))
+      .then(async (_projectName) => {
+        if (typeof _projectName === "undefined") {
+          return;
+        }
+        if (_projectName === "") {
+          vscode.window.showWarningMessage(
+            "You must define a name for the project."
+          );
+          return;
+        }
 
-				if (!projectStorage.exists(projectName)) {					
-					const commands: TerminalCommand[] = [];
-					do {
-						const __command: TerminalCommand = await getNewCommand();
-						commands.push(__command);
-					} while (await isDone());
-					projectStorage.addToProjectList(_projectName, projectPath, commands);
-					projectStorage.save();
+        if (!projectStorage.exists(_projectName)) {
+          projectStorage.addToProjectList(_projectName, projectPath);
+          await addNewCommands(_projectName);
+        } else {
+          const optionAddCommand = {
+            title: USER_OPTIONS.ADD_COMMANDS,
+          } as vscode.MessageItem;
+          const optionCancel = {
+            title: USER_OPTIONS.CANCEL,
+          } as vscode.MessageItem;
 
-					vscode.window.showInformationMessage("Project saved!");
-				} else {
-					const optionAddCommand = {
-						title: "Add Command"
-					} as vscode.MessageItem;
-					const optionCancel = {
-						title: "Cancel"
-					} as vscode.MessageItem;
+          vscode.window
+            .showInformationMessage(
+              "Project already exists!",
+              optionAddCommand,
+              optionCancel
+            )
+            .then(async (option) => {
+              // nothing selected
+              if (typeof option === "undefined") {
+                return;
+              }
 
-					vscode.window.showInformationMessage("Project already exists!", optionAddCommand, optionCancel)
-						.then(async option => {
-							// nothing selected
-							if (typeof option === "undefined") {
-								return;
-							}
+              switch (option.title) {
+                case USER_OPTIONS.ADD_COMMANDS: {
+                  await addNewCommands(_projectName);
+                  return;
+                }
+              }
+            });
+        }
+      });
+  }
 
-							if (option.title === "Add Command") {
+  function editProjects(): void {
+    vscode.workspace.openTextDocument(getProjectFilePath()).then((document) => {
+      vscode.window.showTextDocument(document);
+    });
+  }
 
-							const commands: TerminalCommand[] = [];
-							do {
-								const __command: TerminalCommand = await getNewCommand();
-								commands.push(__command);
-							} while (await isDone());
+  function getProjectFilePath(): string {
+    let projectFile: string;
+    const projectsConfigurationsLocation: string =
+      configurations.projectsConfigurationsLocation;
 
-								projectStorage.addCommands(projectName, commands);
-								projectStorage.save();
-								
-								vscode.window.showInformationMessage("Project saved!");
-								return;
-							} else {
-								return;
-							}
-						});
-				}
-			});
-	};
+    if (projectsConfigurationsLocation !== "") {
+      projectFile = path.join(projectsConfigurationsLocation, PROJECTS_FILE);
+    } else {
+      const appdata =
+        process.env.APPDATA ||
+        (process.platform === "darwin"
+          ? process.env.HOME + "/Library/Application Support"
+          : "/var/local");
+      projectFile = path.join(appdata, "Code", "User", PROJECTS_FILE);
+    }
 
-	function editProjects(): void {
-		vscode.workspace.openTextDocument(getProjectFilePath())
-			.then(document => {
-				vscode.window.showTextDocument(document);
-			});
-	};
+    return projectFile;
+  }
 
-	function getProjectFilePath(): string {
-		let projectFile: string;
-		const projectsConfigurationsLocation: string = configurations.projectsConfigurationsLocation;		
+  async function createTerminal(openNewTerminalWindow, terminal) {
+    let command = openNewTerminalWindow
+      ? "workbench.action.terminal.split"
+      : "workbench.action.terminal.newInActiveWorkspace";
 
-		if (projectsConfigurationsLocation !== "") {
-			projectFile = path.join(projectsConfigurationsLocation, PROJECTS_FILE);
-		} else {
-			const appdata = process.env.APPDATA || (process.platform === "darwin" ? process.env.HOME + "/Library/Application Support" : "/var/local");
-			projectFile = path.join(appdata, "Code", "User", PROJECTS_FILE);
-		}
-		
-		return projectFile;
-	}
+    return new Promise(async (resolve) => {
+      await vscode.commands.executeCommand(command).then(async () => {
+        let listener = vscode.window.onDidOpenTerminal(
+          async (terminalInstance) => {
+            terminalInstance.show();
+            await vscode.commands
+              .executeCommand("workbench.action.terminal.renameWithArg", {
+                name: terminal.name,
+              })
+              .then(async () => {
+                terminalInstance.sendText(terminal.script, true);
+                console.log(openNewTerminalWindow);
+                listener.dispose();
+                resolve();
+              });
+          }
+        );
+      });
+    });
+  }
 
-	function runProject(): void {
-		const workspacePath = vscode.workspace.rootPath;
+  async function runProject() {
+    const workspacePath = vscode.workspace.rootPath;
+    const project: Project = projectStorage.existsWithRootPath(workspacePath);
 
-		const project: Project = projectStorage.existsWithRootPath(workspacePath);
-		let isFirstCommand: boolean = true;
-		for (const command of project.commands) {
-			const terminal = vscode.window.createTerminal(command.name);
-			terminal.sendText(command.script);
-			if (isFirstCommand) {
-				terminal.show();
-				isFirstCommand = false;
-			}
-		}
-	}
+    const { groups = {} } = project;
 
-	async function getNewCommand(): Promise<TerminalCommand> {
-		const _commandName = await vscode.window.showInputBox(commandNameInputBoxOptions());
-		const _commandScript = await vscode.window.showInputBox(commandScriptInputBoxOptions());
-		
-		const _command: TerminalCommand = {
-			name: _commandName,
-			script: _commandScript
-		};
-						
-		return Promise.resolve(_command);
-	}
+    const groupNames = Object.keys(groups);
+    for (let i = 0; i < groupNames.length; i++) {
+      let commands = groups[groupNames[i]];
+      for (let j = 0; j < commands.length; j++) {
+        await createTerminal(j, commands[j]);
+      }
+    }
+  }
 
-	async function isDone(): Promise<boolean> {
-		const optionYes = {
-			title: "Yes"
-		} as vscode.MessageItem;
-		const optionNo = {
-			title: "No"
-		} as vscode.MessageItem;
+  async function addNewCommands(projectName: string): Promise<boolean> {
+    const commands: TerminalCommand[] = [];
+    const _newGroupName = await getGroupName();
+    do {
+      const __command: TerminalCommand = await getNewCommand();
+      commands.push(__command);
+    } while (await isDone());
 
-		const selection = await vscode.window.showInformationMessage(
-			`command is successfully added, do you need to add More?`,
-			optionYes, optionNo
-		);
+    projectStorage.addCommands(projectName, _newGroupName, commands);
 
-		if (selection.title === `Yes`) {
-			return Promise.resolve(true);
-		}else {
-			return Promise.resolve(false);
-		}
-	}
+    projectStorage.save();
+    vscode.window.showInformationMessage("Commands saved!");
+    return Promise.resolve(true);
+  }
+
+  async function getNewCommand(): Promise<TerminalCommand> {
+    const _commandName = await vscode.window.showInputBox(
+      commandNameInputBoxOptions()
+    );
+    const _commandScript = await vscode.window.showInputBox(
+      commandScriptInputBoxOptions()
+    );
+
+    const _command: TerminalCommand = {
+      name: _commandName,
+      script: _commandScript,
+    };
+
+    return Promise.resolve(_command);
+  }
+
+  async function addNewGroup(projectName: string): Promise<string> {
+    const _groupName = await getGroupName();
+    projectStorage.addGroup(projectName, _groupName);
+
+    projectStorage.save();
+    vscode.window.showInformationMessage("Group saved!");
+    return Promise.resolve(_groupName);
+  }
+
+  async function getGroupName(): Promise<string> {
+    const _groupName = await vscode.window.showInputBox(
+      groupNameInputBoxOptions()
+    );
+
+    return Promise.resolve(_groupName);
+  }
+
+  async function isDone(): Promise<boolean> {
+    const optionYes = {
+      title: "Yes",
+    } as vscode.MessageItem;
+    const optionNo = {
+      title: "No",
+    } as vscode.MessageItem;
+
+    const selection = await vscode.window.showInformationMessage(
+      `command is successfully added, do you need to add More?`,
+      optionYes,
+      optionNo
+    );
+
+    if (selection.title === `Yes`) {
+      return Promise.resolve(true);
+    } else {
+      return Promise.resolve(false);
+    }
+  }
 }
--- src/storage.ts
+++ src/storage.ts
@@ -1,36 +1,40 @@
 import fs = require("fs");
-import { Utils } from './utils';
+import { Utils } from "./utils";
 
 const utils_obj = new Utils();
 
 export interface TerminalCommand {
   name: string;
   script: string;
-};
+}
+
+export type Group = Record<string, TerminalCommand[]>;
 
 export interface Project {
   name: string;
   root_path: string;
-  commands: TerminalCommand[];
-};
+  groups: Group;
+}
 
-export interface ProjectList extends Array < Project > {};
+export interface ProjectList extends Array<Project> {}
 
 class ProjectItem implements Project {
-
   public name: string;
   public root_path: string;
-  public commands: TerminalCommand[];
+  public groups: Group;
 
-  constructor(project_name: string, project_root_path: string, terminal_commands: TerminalCommand[]) {
+  constructor(
+    project_name: string,
+    project_root_path: string,
+    groups: Group = {} as Group
+  ) {
     this.name = project_name;
     this.root_path = project_root_path;
-    this.commands = terminal_commands;
+    this.groups = groups;
   }
 }
 
 export class ProjectStorage {
-
   private projectList: ProjectList;
   private filename: string;
 
@@ -39,50 +43,114 @@ export class ProjectStorage {
     this.projectList = [] as ProjectList;
   }
 
-  public addToProjectList(project_name: string, root_path: string, command: TerminalCommand[]): void {    
-    this.projectList.push(new ProjectItem(project_name, root_path, command));
+  // adding project
+  public addToProjectList(project_name: string, root_path: string): void {
+    this.projectList.push(new ProjectItem(project_name, root_path));
     return;
   }
 
-  public removeFormProjectList(project_name: string): Project {
+  // removing project
 
-    const index = utils_obj.getIndexWherePropertyIs(this.projectList, `name`, project_name);
+  public removeFormProjectList(project_name: string): Project {
+    const index = utils_obj.getIndexWherePropertyIs(
+      this.projectList,
+      `name`,
+      project_name
+    );
     return this.projectList.splice(index, 1)[0];
   }
 
-  public addCommands(project_name: string, commands: TerminalCommand[]): void {
-    const index = utils_obj.getIndexWherePropertyIs(this.projectList, `name`, project_name);
+  // adding group
+  public addGroup(project_name: string, groupName: string) {
+    const index = utils_obj.getIndexWherePropertyIs(
+      this.projectList,
+      `name`,
+      project_name
+    );
 
-    this.projectList[index].commands = [...commands];
+    return (this.projectList[index].groups[groupName] = []);
   }
 
-  public removeCommand(project_name: string, command_name: string): void {
-    const project_index = utils_obj.getIndexWherePropertyIs(this.projectList, `name`, project_name);
-    const command_index = utils_obj.getIndexWherePropertyIs(this.projectList[project_index].commands, `name`, command_name)
+  // deleting group
+  public removeGroup(project_name: string, groupName: string) {
+    const index = utils_obj.getIndexWherePropertyIs(
+      this.projectList,
+      `name`,
+      project_name
+    );
 
-    this.projectList[project_index].commands.splice(command_index, 1);
+    const deletedGroup = { ...this.projectList[index].groups[groupName] };
+    delete this.projectList[index].groups[groupName];
+    return deletedGroup;
+  }
+
+  // adding command
+  public addCommands(
+    project_name: string,
+    groupName: string,
+    commands: TerminalCommand[]
+  ): void {
+    const index = utils_obj.getIndexWherePropertyIs(
+      this.projectList,
+      `name`,
+      project_name
+    );
+
+    this.projectList[index].groups[groupName] = commands;
+  }
+
+  // removing command
+  public removeCommand(
+    project_name: string,
+    groupName: string,
+    command_name: string
+  ): void {
+    const project_index = utils_obj.getIndexWherePropertyIs(
+      this.projectList,
+      `name`,
+      project_name
+    );
+    const command_index = utils_obj.getIndexWherePropertyIs(
+      this.projectList[project_index].groups[groupName],
+      `name`,
+      command_name
+    );
+
+    this.projectList[project_index].groups[groupName].splice(command_index, 1);
     return;
   }
 
   public updateRootPath(project_name: string, path: string): void {
-    const index = utils_obj.getIndexWherePropertyIs(this.projectList, `name`, project_name);
+    const index = utils_obj.getIndexWherePropertyIs(
+      this.projectList,
+      `name`,
+      project_name
+    );
 
     this.projectList[index].root_path = path;
   }
 
   public exists(project_name: string): boolean {
     let found: boolean = false;
-    
-    const index = utils_obj.getIndexWherePropertyIs(this.projectList, `name`, project_name);
 
-    if (index > -1 ) {
+    const index = utils_obj.getIndexWherePropertyIs(
+      this.projectList,
+      `name`,
+      project_name
+    );
+
+    if (index > -1) {
       found = true;
     }
     return found;
   }
 
   public existsWithRootPath(root_path: string): Project {
-    const index = utils_obj.getIndexWherePropertyIs(this.projectList, `root_path`, root_path);
+    const index = utils_obj.getIndexWherePropertyIs(
+      this.projectList,
+      `root_path`,
+      root_path
+    );
 
     return this.projectList[index];
   }
@@ -122,14 +190,17 @@ export class ProjectStorage {
   }
 
   public save() {
-    fs.writeFileSync(this.filename, JSON.stringify(this.projectList, null, "\t"));
+    fs.writeFileSync(
+      this.filename,
+      JSON.stringify(this.projectList, null, "\t")
+    );
   }
 
   public map(): any {
-    const newItems = this.projectList.map(item => {
+    const newItems = this.projectList.map((item) => {
       return {
         label: item.name,
-        description: item.root_path
+        description: item.root_path,
       };
     });
     return newItems;
